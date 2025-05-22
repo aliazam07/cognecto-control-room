bf9d75f2209ffde3af8dcdd2c1ae0b114df05d17
saavranAmit <saahib003@gmail.com>
2025-05-22 20:12:57 +0530

    Updated JSON data

--- src/components/VideoPlayer/Overlay.jsx
+++ src/components/VideoPlayer/Overlay.jsx
@@ -30,6 +30,7 @@ export const ErrorBox = () => (
       justifyContent: "center",
       alignItems: "center",
       height: "100%",
+      width: "100%",
       aspectRatio: '16 / 9',
       color: "#696969",
       bgcolor: "#f5f5f5",
